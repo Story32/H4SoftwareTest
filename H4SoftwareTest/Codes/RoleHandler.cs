590bad6907cb10891230b03215a0fef368f8a474
Awes Sana <awes@altinget.dk>
2024-05-13 11:08:19 +0200

    Add project files.

---
+++ H4SoftwareTest/Codes/RoleHandler.cs
@@ -0,0 +1,34 @@
+﻿using H4SoftwareTest.Data;
+using Microsoft.AspNetCore.Identity;
+
+namespace H4SoftwareTest.Codes;
+
+public class RoleHandler
+{
+    public async Task<bool> CreateUserRolesAsync(string user, string role, IServiceProvider serviceProvider)
+    {
+        bool isCreated = false;
+        try
+        {
+            var roleManager = serviceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+            var userManager = serviceProvider.GetRequiredService<UserManager<ApplicationUser>>();
+
+            var userRoleCheck = await roleManager.RoleExistsAsync(role);
+            if (!userRoleCheck)
+            {
+                var roleObj = new IdentityRole(role);
+                await roleManager.CreateAsync(roleObj);
+            }
+
+            ApplicationUser identityUser = await userManager.FindByEmailAsync(user);
+            await userManager.AddToRoleAsync(identityUser, role);
+
+            isCreated = true;
+        }
+        catch(Exception)
+        {
+            isCreated = false;
+        }
+        return isCreated;
+    }
+}
